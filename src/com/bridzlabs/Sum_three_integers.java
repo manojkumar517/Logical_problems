af52dce07fbf2b5d92c5970d7b6732f69aa19743
amanojkumar517 <amanojkumar517@gmail.com>
2023-02-04 12:26:50 +0530

    Added: codes of logical programs

---
+++ src/com/bridzlabs/Sum_three_integers.java
@@ -0,0 +1,19 @@
+package com.bridzlabs;
+
+public class Sum_three_integers {
+    public static void main(String[] args) {
+        int a[] = {3, -1, -7, -4, -5, 9, 10};
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
+                for (int l = j + 1; l < a.length; l++) {
+                    if (a[i] + a[j] + a[l] == 0) {
+                        System.out.println("Found 3 elements whose sum is = 0");
+                        System.out.println("Elements are " + a[i] + " " + a[j] + " " + a[l]);
+                        return;
+                    }
+                }
+            }
+        }
+        System.out.println("Did not find 3 elements whose sum is = 0");
+    }
+}
---
+++ src/com/bridzlabs/Two_dimentional_array.java
@@ -0,0 +1,23 @@
+package com.bridzlabs;
+
+import java.util.Scanner;
+
+public class Two_dimentional_array {
+    public static void main(String[] args) {
+        int matrix[][] = new int[2][2];
+        Scanner Scan = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.println( " enter the value for ( " + i + " ," + j + ") : " );
+                matrix[i][j] = Scan.nextInt();
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
